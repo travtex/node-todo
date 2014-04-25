6d183928f90b95052bad3d5b8f10bb329854bfc6
Travis Flatt <tflatt74@gmail.com>
2014-04-25 13:57:15 -0500

    Mongoose @ modulus test database

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var mongoose = require('mongoose');
 
 // Config
 
-mongoose.connect('mongodb://node:node@mongo.onmodulus.net:27017/uw03mypu');
+mongoose.connect('mongodb://travtex:paradigm@novus.modulusmongo.net:27017/ubohE9ry');
 
 app.configure(function() {
 	app.use(express.static(__dirname + '/public'));
@@ -15,6 +15,58 @@ app.configure(function() {
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
 });
+
+// Model definition
+
+var Todo = mongoose.model('Todo', {
+	text : String
+});
+
+// routes
+
+	//api -------
+	// Get all Todos
+	app.get('/api/todos', function(req, res) {
+		Todo.find(function(err, todos) {
+			if (err)
+				res.send(err)
+			res.json(todos);
+		});
+	});
+
+	app.post('/api/todos', function(req, res) {
+		Todo.create({
+			text: req.body.text,
+			done: false
+		}, function(err, todo) {
+			if(err)
+				res.send(err);
+
+			Todo.find(function(err, todos) {
+				if(err)
+					res.send(err)
+				res.json(todos);
+			});
+		});
+	});
+
+	// delete a todo
+
+	app.delete('/api/todos/:todo_id', function(req, res) {
+		Todo.remove({
+			_id : req.params.todo_id
+		}, function(err, todo) {
+			if(err)
+				res.send(err);
+
+			Todo.find(function(err, todos) {
+				if (err)
+					res.send(err)
+				res.json(todos);
+			});
+		});
+	});
+	
 
 // Listen
 
